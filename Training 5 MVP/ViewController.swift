47b19fa352d38acd7d812dccdf968c89f2a512e2
yudha <yudhapurbajagad@gmail.com>
2019-10-03 09:37:44 +0700

    Initial Commit

---
+++ Training 5 MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training 5 MVP
+//
+//  Created by yudha on 03/10/19.
+//  Copyright © 2019 yudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
